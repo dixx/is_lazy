73df4320d73545c8dd8713ede868973bae363179
dixx <jenzdixx@googlemail.com>
2016-05-11 21:49:54 +0200

    Create aliases.sh

---
+++ git/aliases.sh
@@ -0,0 +1,8 @@
+	
+#!/bin/bash
+git config --global alias.co checkout
+git config --global alias.cm commit
+git config --global alias.a add
+git config --global alias.l log
+git config --global alias.s status
+git config --global alias.p pull
