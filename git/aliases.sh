7ef0efe8a48c116fcef38c53c6032a6f65d27d57
dixx <jenzdixx@googlemail.com>
2016-05-12 13:51:04 +0200

    Update git aliases

--- git/aliases.sh
+++ git/aliases.sh
@@ -2,7 +2,7 @@
 
 git config --global alias.co checkout
 git config --global alias.cm commit
-git config --global alias.a add
+git config --global alias.a add -A
 git config --global alias.l log
 git config --global alias.s status
 git config --global alias.p pull
