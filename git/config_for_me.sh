d9be7ff2a13f0c2b5d743d7a8c9da3ab330682be
dixx <jenzdixx@googlemail.com>
2016-07-05 13:20:34 +0200

    Add .gitignore_global

--- git/config_for_me.sh
+++ git/config_for_me.sh
@@ -3,3 +3,4 @@
 git config --global push.default simple
 git config --global user.name "dixx"
 git config --global user.email "jenzdixx@googlemail.com"
+git config --global core.excludesfile ~/.gitignore_global
