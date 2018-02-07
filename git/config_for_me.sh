a0cdd5d849240774ebaba94aacb6d8b583a7fb18
dixx <jens.zobel@injixo.com>
2018-02-07 12:15:26 +0100

    Add colored white spaces to git diff

--- git/config_for_me.sh
+++ git/config_for_me.sh
@@ -4,3 +4,4 @@ git config --global push.default simple
 git config --global user.name "dixx"
 git config --global user.email "jenzdixx@googlemail.com"
 git config --global core.excludesfile ~/.gitignore_global
+git config --global color.diff.whitespace "red reverse"
