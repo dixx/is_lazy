c736ebe5252891030aaaf248a40e61989a6bf1ed
dixx <jens.zobel@injixo.com>
2018-04-18 12:57:59 +0200

    Update git log alias

--- git/aliases.sh
+++ git/aliases.sh
@@ -4,7 +4,7 @@ git config --global alias.co checkout
 git config --global --replace-all alias.coma 'checkout master'
 git config --global alias.cm commit
 git config --global --replace-all alias.a 'add -A'
-git config --global alias.l log
+git config --global alias.l "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.s status
 git config --global alias.p pull
 git config --global --replace-all alias.pp 'pull --all --prune'
